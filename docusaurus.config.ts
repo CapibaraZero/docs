de094b890f76109d6a5c1956ab805c42ca6a8726
Andrea Canale <andreockx@proton.me>
2024-11-06 01:00:15 +0100

    New domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://capibarazero.github.io/',
+  url: 'https://docs.capibarazero.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
